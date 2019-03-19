ebe104549b9fbef1e87c0da4355e1c98f5b43102
lingchaolin <lingchaolin@yeah.net>
2019-03-19 22:55:37 +0800

    2019-03-19_22:55:35

--- gitpust.py
+++ gitpust.py
@@ -4,7 +4,7 @@ import os
 import time
 
 os.system('git add .')
-now = time.strftime('%Y-%m-%d ',time.localtime(time.time()))
+now = time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time()))
 os.system('git pull origin')
 os.system('git commit -m '+str(now))
 os.system('git push origin master')
